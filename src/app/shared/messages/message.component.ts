312bf8bc6fbe555d561ae7c949c2164488366826
uncol <d.a.zakharov@gmail.com>
2017-08-02 12:26:48 +0300

    add message.component

---
+++ src/app/shared/messages/message.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Message } from '../../model/message';
+import { MessageService } from '../../services/message.service';
+
+@Component({
+    selector: 'bi-message',
+    template: `
+        <div class="alert {{ message.type }} alert-dismissible message"
+             role="alert">
+            <button type="button" class="close" aria-label="Close" (click)="close(message)">
+                <span>&times;</span>
+            </button>
+            <div>{{message.text}}</div>
+        </div>
+    `,
+    styleUrls: ['./messages.component.scss']
+})
+export class MessageComponent implements OnInit {
+    @Input()
+    public message: Message;
+
+    constructor(private messagesService: MessageService) {
+    }
+
+    ngOnInit() {
+        setTimeout(() => this.messagesService.removeMessage(this.message), 5000);
+    }
+
+    close(message: Message) {
+        this.messagesService.removeMessage(message);
+    }
+}
