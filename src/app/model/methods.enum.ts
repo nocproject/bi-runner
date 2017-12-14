7dccff4587ac13b21e0bc2b11291116e4fc56544
uncol <d.a.zakharov@gmail.com>
2017-12-14 11:33:44 +0300

    upgrade dc to 2.1.9

--- src/app/model/methods.enum.ts
+++ src/app/model/methods.enum.ts
@@ -1,16 +1,17 @@
 export class Methods {
     static QUERY = 'query';
     static LIST_DASHBOARDS = 'list_dashboards';
+    static LIST_DATASOURCES = 'list_datasources';
     static LIST_GROUPS = 'list_groups';
     static LIST_USERS = 'list_users';
     static GET_DASHBOARD = 'get_dashboard';
     static GET_USER_ACCESS = 'get_user_access';
-    static SET_DASHBOARD = 'set_dashboard';
-    static REMOVE_DASHBOARD = 'remove_dashboard';
     static GET_DATASOURCE_INFO = 'get_datasource_info';
     static GET_DASHBOARD_ACCESS = 'get_dashboard_access';
     static SET_DASHBOARD_ACCESS = 'set_dashboard_access';
     static GET_HIERARCHY = 'get_hierarchy';
+    static SET_DASHBOARD = 'set_dashboard';
     static SET_DASHBOARD_ACCESS_GROUP = 'set_dashboard_access_group';
     static SET_DASHBOARD_ACCESS_USER = 'set_dashboard_access_user';
+    static REMOVE_DASHBOARD = 'remove_dashboard';
 }
