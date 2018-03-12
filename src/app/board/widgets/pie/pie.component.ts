d5c945ddbf33590cf7d5b0300ac64f79b2f5879a
uncol <d.a.zakharov@gmail.com>
2018-03-12 22:36:06 +0300

    pie legend make text with ellipsis

--- src/app/board/widgets/pie/pie.component.ts
+++ src/app/board/widgets/pie/pie.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import * as crossfilter from 'crossfilter';
+import * as d3 from 'd3';
 import * as dc from 'dc';
 import { BaseMixin, Legend, PieChart } from 'dc';
 
@@ -18,11 +19,11 @@ export class PieComponent extends WidgetComponent {
         const chart: PieChart = dc.pieChart(`#${this.data.cell.name}`);
         const ndx = crossfilter(response.zip(false));
         const dimension = ndx.dimension(d => new Value(d[Object.keys(d)[0]], d.name));
-        const values = dimension.group().reduceSum(d => d.cnt);
+        const values = dimension.group().reduceSum(d => d['cnt']);
 
         const width = this.wrapperView.nativeElement.scrollWidth;
         const height = this.data.cell.height - (this.isSelectable() ? 50 : 0);
-        const legendWidth = width - height;
+        const legendWidth = width - 1.5 * height;
         const offset = 30;
         const legend: Legend = dc.legend();
 
@@ -34,9 +35,18 @@ export class PieComponent extends WidgetComponent {
         legend.itemHeight(13);
         legend.gap(5);
         legend.autoItemWidth(true);
-        legend.legendWidth(legendWidth);
-        legend.itemWidth(legendWidth - 5);
-        legend.legendText(d => d.name.desc);
+        legend.legendText(function (d) {
+            const self = d3.select(this);
+            self.text(d.name.desc);
+            let textLength = this.getComputedTextLength();
+            let text = self.text();
+            while (textLength > legendWidth && text.length > 0) {
+                text = text.slice(0, -1);
+                self.text(text + '...');
+                textLength = this.getComputedTextLength();
+            }
+            return self.text();
+        });
 
         chart.width(width);
         chart.height(height);
